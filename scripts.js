41bb69b46ca314c4d6257e5201d472848f983a9e
Gleb Vyganovsky <gvyganovsky@gmail.com>
2025-12-16 15:20:15 +0300

    Add slider to section 'Photos'

--- scripts.js
+++ scripts.js
@@ -1,7 +1,41 @@
+/*
+    ----------------------------------
+    1. Photos
+    ----------------------------------
+*/
+
+var swiper = new Swiper('.photos__swiper', {
+  loop: true,
+  speed: 1000,
+  lazy: true,
+  autoplay: true,
+  slidesPerView: 1,
+  delay: 3000,
+  keyboard: true,
+  mousewheel: true,
+  spaceBetween: 30,
+  breakpoints: {
+    1320: {
+      slidesPerView: 3,
+      centeredSlides: false
+    }
+  },
+
+  pagination: {
+    el: ".photos__pagination",
+    dynamicBullets: true,
+  },
+});
+/*
+    ----------------------------------
+    2. Questions
+    ----------------------------------
+*/
+
 const questions = document.querySelectorAll('[data-question-item]');
 
 questions.forEach(question => {
   question.addEventListener('click', () => {
-    question.classList.toggle("questions__item_active");
+    question.classList.toggle('questions__item_active');
   });
 });
